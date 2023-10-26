55c61c438e38b8d93d31ba7f713e228bbf9a0500
Aneesh M Bhat <61986514+Aneesh-M-Bhat@users.noreply.github.com>
2023-10-26 23:41:15 +0530

    Time: 2699 ms (7.14%), Space: 143.4 MB (7.14%) - LeetHub

--- 0843-binary-trees-with-factors/0843-binary-trees-with-factors.js
+++ 0843-binary-trees-with-factors/0843-binary-trees-with-factors.js
@@ -5,8 +5,10 @@ var numFactoredBinaryTrees = function (arr) {
   for(let i=0;i<arr.length;i++){
       map[arr[i]] = 1;
       for(let j=0;j<i;j++){
-          if(arr[i]%arr[j]==0&&map[arr[i]/arr[j]]){
-              map[arr[i]] += map[arr[j]]*map[arr[i]/arr[j]];
+          let factor = map[arr[j]];
+          let otherFactor = map[arr[i]/arr[j]];
+          if(arr[i]%arr[j]==0&&otherFactor){
+              map[arr[i]] += factor*otherFactor;
               
           }
       }
