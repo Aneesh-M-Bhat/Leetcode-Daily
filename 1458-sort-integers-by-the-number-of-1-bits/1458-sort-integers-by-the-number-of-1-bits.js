4f36266fc1aa89e1085fc49ba7f3fec52206d2da
Aneesh M Bhat <61986514+Aneesh-M-Bhat@users.noreply.github.com>
2023-10-30 12:51:26 +0530

    Time: 69 ms (75%), Space: 44.1 MB (95.83%) - LeetHub

--- 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.js
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.js
@@ -2,20 +2,20 @@
  * @param {number[]} arr
  * @return {number[]}
  */
-function count(str) {
-  var sum = 0;
-  
-  for (var i = 0, length = str.length; i < length; i++) {
-    sum += Number(str[i]);
+ 
+function count(num) {
+  let count = 0;
+  while(num>0){
+      count++;
+      num &= (num-1);
   }
-  
-  return sum;
+  return count;
 }
 
 var sortByBits = function(arr) {
     arr.sort((a,b)=>{
-        let x = count((a >>> 0).toString(2));
-        let y = count((b >>> 0).toString(2));
+        let x = count(a);
+        let y = count(b);
         return x==y?a-b:x-y;
     });
     return arr;
